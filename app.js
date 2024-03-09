2ec1569151abfc04f4bef6b9065fdfa97f04608f
datpkamw1998 <datpkamw1998@gmail.com>
2024-03-09 14:42:30 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-
+const contactsRouter = require("./app/routes/contact.route");
 app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "welcome to  contact book application" })
 });
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
